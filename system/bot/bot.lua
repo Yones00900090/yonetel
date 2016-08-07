ca362f4d7a9aa42a2b4d0663ba90dbd0f1fa6a07
Yones00900090 <yones.sabri81@gmail.com>
2016-08-07 20:48:31 +0430

    Update bot.lua

--- system/bot/bot.lua
+++ system/bot/bot.lua
@@ -244,7 +244,7 @@ function create_config( )
     "whitelist"
     },
     vip_users = {59561185}, --vip users
-    sudo_users = {253566671},--Sudo users
+    sudo_users = {59561185},--Sudo users
     support_gp = {111111111},--Support id
     moderation = {data = 'system/data/adv.json'},
     about_text = [[*IN THE NAME OF ALLAH*
